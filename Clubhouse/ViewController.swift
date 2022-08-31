8fd2d4f6f1f4f8e69f7ca0eb182ac4d025f67fec
AW <veselovwork@gmail.com>
2022-09-01 00:37:20 +0300

    Initial Commit

---
+++ Clubhouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clubhouse
+//
+//  Created by Andrey on 01.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
